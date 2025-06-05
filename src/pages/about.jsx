ed446669b460c88e01ccba8f6552f443b874eb04
Jonathan Alabi <alabijonathan600@gmail.com>
2025-06-05 02:25:36 +0100

    About AktiveGym

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,28 @@
+import "../styles/pages/about.scss";
+
 const About = () => {
   return (
-    <div>abjk</div>
-  )
-}
+    <div className="about-container">
+      <div className="about-text">
+        <p className="about-header">About AktiveGym</p>
 
-export default About
+        <h1>
+          STRONGER TOGETHER. <br /> BUILT FOR EVERYBODY.
+        </h1>
+        <p>
+          At AktiveGym, we’re redefining fitness for <br /> students,
+          professionals, and athletes through <br /> expert coaching, community,
+          and a tailored <br />
+          wellness experience. Whether you're just <br /> starting out or
+          chasing elite performance
+        </p>
+      </div>
+
+      <div className="about-image">
+        <img src="src/assets/images/about.png" alt="About Image" />
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,20 +1,61 @@
 import Navbar from "../pageLayouts/navBar";
 import "../styles/pages/landingPage.scss";
+import CustomButton from "../components/CustomButton";
+import { Link as RouterLink } from "react-router-dom";
+import About from "./about";
+import MeetTrainers from "./meetTrainers";
 
 const LandingPage = () => {
   return (
-    <div>
+    <>
       <div className="hero-background">
         <Navbar />
         <section className="hero-section">
+          <h1 className="hero-title">
+            TRAIN SMARTER.{" "}
+            <span>
+              <br />
+            </span>{" "}
+            LIVE LONGER.
+          </h1>
+          <p className="hero-subtitle">
+            Welcome to AktiveGym – your all-in-one <br />
+            destination for fitness, wellness, and lifestyle <br />
+            transformation.
+          </p>
 
+          <CustomButton component={RouterLink} size="large" to="/get-started">
+            Explore Plan
+          </CustomButton>
+        </section>
+
+        <section className="sponsor-section">
+          <div className="sponsor-slider">
+            <div className="slide-track">
+              <img src="src/assets/images/nike.png" alt="NIKE" />
+              <img src="src/assets/images/myprotein.png" alt="MY PROTEIN" />
+              <img src="src/assets/images/bulk.png" alt="BULK" />
+              <img src="src/assets/images/gymshark.png" alt="GYM SHARK" />
+              <img src="src/assets/images/freshfit.png" alt="FRESH FIT" />
+
+              <img src="src/assets/images/nike.png" alt="NIKE" />
+              <img src="src/assets/images/myprotein.png" alt="MY PROTEIN" />
+              <img src="src/assets/images/bulk.png" alt="BULK" />
+              <img src="src/assets/images/gymshark.png" alt="GYM SHARK" />
+              <img src="src/assets/images/freshfit.png" alt="FRESH FIT" />
+            </div>
+          </div>
         </section>
       </div>
 
       <section id="about" style={{ minHeight: "100vh", padding: "2rem" }}>
-        <h2>About Us</h2>
-        <p>Some content about your service.</p>
+        <About />
       </section>
+
+      <section id="meet-trainers" style={{ minHeight: "100vh", padding: "2rem" }}>
+        <MeetTrainers/>
+      </section>
+
 
       <section
         id="how-it-works"
@@ -28,7 +69,7 @@ const LandingPage = () => {
         <h2>Pricing</h2>
         <p>Your pricing details go here.</p>
       </section>
-    </div>
+    </>
   );
 };
 
